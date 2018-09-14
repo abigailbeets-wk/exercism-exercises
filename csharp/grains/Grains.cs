15c6d7a1f0e71d148d4a1a3df6c76f4aeb996f26
Abigail Beets <abigail.beets@workiva.com>
2018-09-14 10:46:43 -0400

    grains solution

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if(n <= 0 || n > 64) {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return (ulong)Math.Pow(2, n-1);
+    }
+
+    public static ulong Total()
+    {
+        ulong total = 0;
+        for(int i = 64; i > 0; i--) {
+            total += Square(i);
+        }
+
+        return total;
+    }
+}
