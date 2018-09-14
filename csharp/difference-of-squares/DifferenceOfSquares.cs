17151027a8e76029b8e29a6419fa607b0313c525
Abigail Beets <abigail.beets@workiva.com>
2018-09-14 10:24:49 -0400

    non-LINQ solution for difference-of-squares

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -4,16 +4,29 @@ public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sum = 0;
+        while(max > 0) {
+            sum += max;
+            max--;
+        }
+
+        return sum * sum;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sumOfSquares = 0;
+        while(max > 0) {
+            sumOfSquares += max * max;
+            max--;
+        }
+
+        return sumOfSquares;
     }
 
+    // square of sum - sum of squares
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
 }
